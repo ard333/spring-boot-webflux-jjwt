509b989f358305bf0453275af008a27c2953c929
ard333 <ardiansyah@ard333.com>
2018-12-25 14:48:22 +0700

    update url

--- src/main/java/com/ard333/springbootwebfluxjjwt/rest/ResourceREST.java
+++ src/main/java/com/ard333/springbootwebfluxjjwt/rest/ResourceREST.java
@@ -19,19 +19,19 @@ import reactor.core.publisher.Mono;
 @RestController
 public class ResourceREST {
 	
-	@RequestMapping(value = "resource/user", method = RequestMethod.GET)
+	@RequestMapping(value = "/resource/user", method = RequestMethod.GET)
 	@PreAuthorize("hasRole('USER')")
 	public Mono<ResponseEntity<?>> user() {
 		return Mono.just(ResponseEntity.ok(new Message("Content for user")));
 	}
 	
-	@RequestMapping(value = "resource/admin", method = RequestMethod.GET)
+	@RequestMapping(value = "/resource/admin", method = RequestMethod.GET)
 	@PreAuthorize("hasRole('ADMIN')")
 	public Mono<ResponseEntity<?>> admin() {
 		return Mono.just(ResponseEntity.ok(new Message("Content for admin")));
 	}
 	
-	@RequestMapping(value = "resource/user-or-admin", method = RequestMethod.GET)
+	@RequestMapping(value = "/resource/user-or-admin", method = RequestMethod.GET)
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public Mono<ResponseEntity<?>> userOrAdmin() {
 		return Mono.just(ResponseEntity.ok(new Message("Content for user or admin")));
